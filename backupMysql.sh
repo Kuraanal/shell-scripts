d9fa904c02fe463bfee89aa2c693dfb9a62ec729
Kuraanal <eric.agnel@gmail.com>
2023-08-14 18:45:26 +0800

    Change strings and comments from French to English.

--- backupMysql.sh
+++ backupMysql.sh
@@ -1,132 +1,133 @@
 #!/bin/bash -x
 ###########################################
-# Fonction : Sauvegarde des bases Mysql
-#            sur serveur LINUX
+# Fonction : MYSQL Database backup script
 #
 # Version : 1.5
-# Date de modification : 03/12/2012
+# Last modification : 03/12/2012
 # Auteur : AGNEL Eric ( eric.agnel@gmail.com )
 ###########################################
 
 
 
 
-launchHelp=0 # Definit la variable de test sur false par defaut
+launchHelp=0 # initialise help to false
 
-# Chemins de base. Utilises par le script si aucun argument ne les specifie
+# Base directories
 logPath="/var/log/$0.log"
 savePath="/var/local/dumps/"
 
-#Fonction d'affichage de l'aide
-afficheHelp()
+# Print the help
+getHelp()
 {
-	echo "Options du script : backupMysql.sh -u:XXX "
-    echo "-h	- Affiche l'aide d'utilisation du script."
-	echo "-p	- Definit le mot de passe a utiliser pour la connexion aux bases. ( -p:PASSWORD )"
-	echo "-u	- \"OBLIGATOIRE\" Definit le compte pour la connexion aux bases. ( -u:USER )"
-	echo "-db	- Definit le nom de la base a sauvegarder. ( -db:DATABASENAME )"
-	echo "-log	- Chemin du fichier LOG. ( -log:LOGPATH )"
-	echo "-s	- Chemin du dossier de destination des sauvegardes. ( -s:SAVEPATH )"
+	echo "Script options : backupMysql.sh -u:XXX "
+    echo "-h	- Print this help dialog."
+	echo "-p	- Define the password to use for the database connection. ( -p:PASSWORD )"
+	echo "-u	- Define the username to use for the connection. ( -u:USER )"
+	echo "-db	- Define the database name to backup. All databases will be exported if empty ( -db:DATABASENAME )"
+	echo "-log	- Define the path to the log file. ( -log:LOGPATH )"
+	echo "-s	- Define the path for the backup directory. ( -s:SAVEPATH )"
 	echo ""
-	echo "Seul l'argument \"-u:USER\" est obligatoire. Les autres ont une valeur de base ou ne sont pas necessaires."
+	echo "All arguments are optional except for the user. The minimum command is backupMysql.sh -u:XXX"
 }
 
 if [ $# -eq 0 ]
 then
-	echo "Aucun argument fournit. Veuillez fournir au moins un compte de connexion a la base" >> $logPath
+	echo "No arguments provided. You must speficy at least the user to use for the connection." >> $logPath
 	echo ""
-	afficheHelp
+	getHelp
 else
-	# Verif si l'argument -log est specifie
+	# First check if the log arguments is set
 	for args in "$@"
 	do
 		if [[ "$args" =~ ^[-]log[:][a-zA-Z0-9\/]{1,} ]]
 		then
-			logPath=$(expr substr "$args" 6 $(expr length "$args")) # Modifie le chemin d'acces du fichier LOG
-			echo "Fichier log definie a : $logPath"
+			logPath=$(expr substr "$args" 6 $(expr length "$args"))
+			echo "Log file set to : $logPath"
 			break
 		fi
 	done
 	
 	echo "=================================" >> $logPath
-	echo "$(date "+%d-%m-%Y %H:%M") - Lancement de la procedure de sauvegarde des bases MySQL" >> $logPath
+	echo "$(date "+%d-%m-%Y %H:%M") - Starting database backup" >> $logPath
 
-	for arg in "$@" #Recuperation des arguments pour configuration du Job.
+	for arg in "$@" #Get the arguments for job setup
 	do
 		if [[ "$arg" =~ ^[-]db[:][a-zA-Z0-9]{1,} ]]
 		then
-			database=$(expr substr "$arg" 5 $(expr length "$arg")) #Specifie le nom de la base a sauvegarder
+			database=$(expr substr "$arg" 5 $(expr length "$arg")) # set the name of the database to be saved
 		elif [[ "$arg" =~ ^[-]u[:][a-zA-Z0-9]{1,} ]]
 		then
-			user=$(expr substr "$arg" 4 $(expr length $arg)) # Specifie le compte a utiliser pour la connexion a la base
+			user=$(expr substr "$arg" 4 $(expr length $arg)) # Set the user to be used for connection to the database
 		elif [[ "$arg" =~ ^[-]p[:][a-zA-Z0-9]{1,} ]]
 		then
-			password=$(expr substr "$arg" 4 $(expr length "$arg")) # Specifie le mot de passe a utiliser avec le compte de connexion
+			password=$(expr substr "$arg" 4 $(expr length "$arg")) # Set the password to be used for connection to the database
 		elif [[ "$arg" == "-h" ]]
 		then
-			launchHelp=1 # Specifie la variable d'affichage d'aide a True
+			launchHelp=1 # set the help menu to true
 		elif [[ "$arg" =~ ^[-]s[:][a-zA-Z0-9\/]{1,}[/]$ ]]
 		then
-			savePath=$(expr substr "$arg" 4 $(expr length "$arg")) # Modifie le chemin d'acces des fichiers de sauvegarde
+			savePath=$(expr substr "$arg" 4 $(expr length "$arg")) # Set the path for the backup files
 		else
-			echo "$0 - $(date "+%d-%m-%Y %H:%M") - Argument $arg non reconnu. Utilisez l'argument -h pour plus d'informations." >> "/var/log/messages" 
+			echo "$0 - $(date "+%d-%m-%Y %H:%M") - Argument $arg not recognised. Use -h for more informations." >> "/var/log/messages" 
 		fi
 	done
 
 
-	if [[ $launchHelp != 1 ]] # Verifis si l'aide est demandee. Si oui, affiche l'aie, sinon cotinu le Job.
+	if [[ $launchHelp != 1 ]] # Print the help if asked for.
 	then
-		if [[ "$user" != "" ]] # Verifie qu'un compte a bien ete specifie pour poursuivre le job
+		if [[ "$user" != "" ]] # Check if a user is defined
 		then
-			echo "$(date "+%d-%m-%Y %H:%M") - Un compte a ete fournit. Poursuite du Job" >> $logPath
-
-	        if [[ "$password" != "" ]] # Verifie si un mot de passe est necessaire a la connexion
+	        if [[ "$password" != "" ]] # is a password provided? if yes use it.
             then
-               	echo "$(date "+%d-%m-%Y %H:%M") - Mot de passe fournit. Utilisation du mot de passe avec le compte $user" >> $logPath
+               	echo "$(date "+%d-%m-%Y %H:%M") - A password provided for the user $user" >> $logPath
 				mysqlCommandPass="-p$password"
 				mysqldCommandPass="--password=$password"
             else
-                echo "$(date "+%d-%m-%Y %H:%M") - Aucun mot de passe fournit. Aucun mot de passe ne sera pas utilise pour la conexion a la base" >> $loPath
+                echo "$(date "+%d-%m-%Y %H:%M") - No password provided, connection will be made without one." >> $loPath
 				mysqlCommandPass=""
 				mysqldCommandPass=""
             fi
 		
-			if [[ "$database" != "" ]] # Verifie qu'une base est demandee pour la sauvegarde. Si oui sauvegarde uniquement cette base, sinon sauvegarde toutes les bases hors bases Systeme
+
+			# Check for a database name.
+			# If provided, the backup will save this database only. 
+			# If not provided, will backup all databases.
+			if [[ "$database" != "" ]] 
 			then
-				echo "$(date "+%d-%m-%Y %H:%M") - Demande de sauvegarde de la base $database. Verification de sa presence sur le serveur" >> $logPath
+				echo "$(date "+%d-%m-%Y %H:%M") - Database $database will be saved. Checking for the database on the server." >> $logPath
 				
-				# Verifis si la base passee en parametre existe bien dans MySQL
+				# Check if the database exist.
 				for i in $(mysql -u $user $mysqlCommandPass -e "SHOW DATABASES;")
 				do
 					if [[ $i == $database ]]
 	              	then
-						echo "$(date +%d/%m/%Y-%H:%M) - Base presente. Sauvegarde de la base $i vers $savePath$DATE-$i.sql" >> $logPath
+						echo "$(date +%d/%m/%Y-%H:%M) - The database is present. Will backup $i to $savePath$DATE-$i.sql" >> $logPath
 						mysqldump --databases "$i" --user="$user" "$mysqldCommandPass" --lock-tables > "$savePath$(date +%d-%m-%Y)-$i.sql"
 					else
-						echo "Base non trouvee sur le serveur. Abandon de la sauvegarde."
+						echo "Database notfound. backup aborted."
 					fi
 				done
 			else
-				echo "$(date "+%d-%m-%Y %H:%M") - Aucun nom de Base specifie. Sauvegarde de toutes les bases" >> $logPath
+				echo "$(date "+%d-%m-%Y %H:%M") - No database name given. Will backup every databases on the server" >> $logPath
 				for i in $(mysql -u $user $mysqlCommandPass -e "SHOW DATABASES;")
 				do
-					# Sauvegarde toutes les bases ne faisant pas partie des base standard de MySQL
+					# Backup all databases except for MYSQL default databases
 					if [[ "$i" != "performance_schema" && "$i" != "Database" && "$i" != "information_schema" && "$i" != "test" && "$i" != "mysql" ]]
 		            then
-		                    echo "$(date "+%d-%m-%Y %H:%M") - Sauvegarde de la base $i vers $savePath$(date +%d-%m-%Y)-$i.sql" >> $logPath
+		                    echo "$(date "+%d-%m-%Y %H:%M") - The database $i has been saved to $savePath$(date +%d-%m-%Y)-$i.sql" >> $logPath
                 		    mysqldump --databases "$i" --user="$user" "$mysqldCommandPass" --lock-tables > "$savePath$(date +%d-%m-%Y)-$i.sql"
                 	fi
 				done
 			fi
 		else
-			echo "$(date "+%d-%m-%Y %H:%M") - Aucun nom d'utilisateur n'a ete specifie. Abandon de la sauvegarde" >> $logPath
+			echo "$(date "+%d-%m-%Y %H:%M") - No username specified. Backup aborted." >> $logPath
 		fi
 
-		echo "$(date "+%d-%m-%Y %H:%M") - Fin de la procedure de sauvegarde" >> $logPath
+		echo "$(date "+%d-%m-%Y %H:%M") - Stoping database backup. " >> $logPath
 		echo "=================================" >> $logPath
 		echo ""
 	else
-		afficheHelp
+		getHelp
 	fi
 fi
 
